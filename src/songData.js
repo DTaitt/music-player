d10d9f0474d957b2430ed42bd7e9d6127ffe5268
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-07-17 13:11:13 -0400

    added more songs to db

--- src/songData.js
+++ src/songData.js
@@ -4,6 +4,9 @@
 
 const prettyWings = require('./audio/03 Pretty Wings.mp3');
 const shapeOfYou = require('./audio/04 - Shape of You.mp3');
+const fuGeeLa = require('./audio/06. Fu-Gee-La.mp3');
+const luv = require('./audio/14. Luv.mp3');
+const someoneLikeYou = require('./audio/17 Adele - Someone Like You (Live Acoustic).mp3');
 
 const songData = [
   {
@@ -15,7 +18,22 @@ const songData = [
     'title': 'Shape of You',
     'album': '÷',
     'audio': shapeOfYou
-  }
+  },
+  {
+    'title': 'Fu-Gee-La',
+    'album': 'The Score',
+    'audio': fuGeeLa
+  },
+  {
+    'title': 'Luv',
+    'album': 'I Told You',
+    'audio': luv
+  },
+  {
+    'title': 'Someone Like You',
+    'album': '21',
+    'audio': someoneLikeYou
+  },
 ]
 
 export default songData;
