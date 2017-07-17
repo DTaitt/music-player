7e7eb04a61271b89ad177c63f62341da8006b8c4
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-07-17 11:35:52 -0400

    created song db

---
+++ src/songData.js
@@ -0,0 +1,17 @@
+import 'prettyWings' from './audio/03 Pretty Wings.mp3';
+import 'shapeOfYou' from '04 - Shape of You.mp3';
+
+const songData = [
+  {
+    'title': 'Pretty Wings',
+    'album': 'BLACKsummers\'night',
+    'audio'; prettyWings
+  },
+  {
+    'title': 'Shape of You',
+    'album': '÷',
+    'audio'; shapeOfYou
+  }
+]
+
+export default songData;
