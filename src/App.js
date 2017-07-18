24895c48c1cdce7984cd6529b3ac2999067c149b
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-07-18 01:24:08 -0400

    moved state here from Songlist

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-// import songData from '../songData.js';
-// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+//import song data
+import songData from './songData.js';
+
+//import material ui theme
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+//import components
 import Header from './components/Header.jsx';
 import Songlist from './components/Songlist.jsx';
 
 class App extends Component {
 
-  // constructor() {
-  //   this.state = {
-  //     songData: songData,
-  //   }
-  // }
+  constructor() {
+    super()
+    this.state = {
+      songData: songData,
+      // isLiked: null,
+    }
+  }
 
   render() {
     return (
-      <div className="App">
-        <MuiThemeProvider>
+      <MuiThemeProvider>
+        <div className="App">
           <Header />
-          <Songlist />
-        </MuiThemeProvider>
-      </div>
+          <Songlist songData = { this.state.songData } />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
