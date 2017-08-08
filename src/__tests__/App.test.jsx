59e0dd0f2606785b97419383bec14cc6f3c608b6
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-08-08 13:02:25 -0400

    tweaked data loading testing

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 
 //import components
 import AppClass from './../App';
 
-const App = shallow(<AppClass />, { lifecycleExperimental: true });
+
+const setup = (propOverrides) => {
+  const props = Object.assign({
+    onChange: jest.fn(),
+    addToPlaylist: jest.fn(),
+  }, propOverrides)
+
+  const App = shallow(<AppClass {...props} />, { lifecycleExperimental: true })
+
+  return {
+    props,
+    App,
+    songData: App.state('songData'),
+    playlist: App.state('playlist'),
+    MuiThemeProvider: App.find('MuiThemeProvider'),
+    Header: App.find('Header'),
+    TabBody: App.find('TabBody'),
+  }
+}
+
+const { props } = setup();
+const { App } = setup();
+const { songData } = setup();
+const { playlist } = setup();
+const { MuiThemeProvider } = setup();
+const { Header } = setup();
+const { TabBody } = setup();
 
 describe('App.js', () => {
-  describe('rendering', () => {
+  describe('renders', () => {
     test('App component', () => {
       expect(App.exists()).toBe(true);
     })
     test('MuiThemeProvider component', () => {
-      expect(App.find('MuiThemeProvider').exists()).toBe(true);
+      expect(MuiThemeProvider.exists()).toBe(true);
     })
     test('Header component', () => {
-      expect(App.find('Header').exists()).toBe(true);
+      expect(Header.exists()).toBe(true);
     })
     test('TabBody component', () => {
-      expect(App.find('TabBody').exists()).toBe(true);
+      expect(TabBody.exists()).toBe(true);
     })
   })
-  describe('loads', () => {
-    test('song data', () => {
-      expect(App.state('songData')).not.toBe(null);
+  describe('calls', () => {
+    test('fetchSongData', () => {
+      expect(songData).not.toBeNull();
     })
   })
 })
