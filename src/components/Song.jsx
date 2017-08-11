fa3cdf7377947957e227e0e63bc8edf4623b2b1a
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-08-10 21:02:59 -0400

    misc tweaks

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -49,18 +49,11 @@ class Song extends Component {
 
   render() {
     return (
-      <li>
+      <li className="song">
         <Paper zDepth={3} rounded={false} className='song'>
           <SongInfo artist={ this.props.artist } title={ this.props.title } album={ this.props.album } />
           <div className="audio-player">
-            {/*<audio className={ this.props.audio } controls>
-              <source src={ this.props.audio } type={ `audio/${ this.props.filetype }` }/>
-            </audio>*/}
-            {/*<FontIcon className="material-icons play-btn" onTouchTap={ () => this.props.audio.play() }>play_circle_filled</FontIcon>
-            <FontIcon className="material-icons play-btn">pause_circle_filled</FontIcon>*/}
-            <AudioPlayer
-              src={ this.props.audio }
-            />
+            <AudioPlayer src={ this.props.audio } />
           </div>
           <InteractionIcons
             likeSong={ this.likeSong }
