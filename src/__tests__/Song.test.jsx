8eaf480b58ee0e7abd73cea20b8122a2b3b1d181
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-08-10 21:03:24 -0400

    created render testing

---
+++ src/__tests__/Song.test.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+//import components
+import SongClass from './../components/Song';
+
+
+const setup = (propOverrides) => {
+  // const props = Object.assign({
+  //   Song: [0, 1, 2, 3],
+  // }, propOverrides)
+
+  const Song = shallow(<SongClass /*{...props} *//>, { lifecycleExperimental: true })
+
+  return {
+    // props,
+    Song,
+    SongLi: Song.find('li.song'),
+    Paper: Song.find('Paper'),
+    SongInfo: Song.find('SongInfo'),
+    AudioPlayerDiv: Song.find('div.audio-player'),
+    AudioPlayer: Song.find('AudioPlayer'),
+    InteractionIcons: Song.find('InteractionIcons'),
+  }
+}
+
+
+const { props } = setup();
+const { Song } = setup();
+const { slideIndex } = setup();
+const { SongLi } = setup();
+const { Paper } = setup();
+const { SongInfo } = setup();
+const { AudioPlayerDiv } = setup();
+const { AudioPlayer } = setup();
+const { InteractionIcons } = setup();
+
+function renderTester(name, component) {
+  test(`${name} component`, () => {
+      expect(component.exists()).toBe(true);
+    })
+}
+
+describe('Song.js', () => {
+  describe('renders', () => {
+    renderTester('Song', Song)
+    renderTester('SongLi', SongLi)
+    renderTester('Paper', Paper)
+    // renderTester('SongInfo', SongInfo)
+    renderTester('AudioPlayerDiv', AudioPlayerDiv)
+    // renderTester('AudioPlayer', AudioPlayer)
+    // renderTester('InteractionIcons', InteractionIcons)
+  })
+})
