c447061d6b001b6e3d7baa2bd10d90bd9452ccf5
Donovan Taitt <Donovan.taitt@yahoo.com>
2017-08-10 21:19:47 -0400

    created render testing

---
+++ src/__tests__/InteractionIcons.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+//import components
+import InteractionIconsClass from './../components/InteractionIcons';
+
+
+const setup = (propOverrides) => {
+  // const props = Object.assign({
+  //   onChange: jest.fn(),
+  // }, propOverrides)
+
+  const InteractionIcons = shallow(<InteractionIconsClass />, { lifecycleExperimental: true })
+
+  return {
+    // props,
+    InteractionIcons,
+    // slideIndex: InteractionIcons.state('slideIndex'),
+    InteractionIconsDiv: InteractionIcons.find('div.interaction-icons'),
+    LikeBtn: InteractionIcons.find('FontIcon.like-btn'),
+    DislikeBtn: InteractionIcons.find('FontIcon.dislike-btn'),
+    PlaylistBtn: InteractionIcons.find('FontIcon.playlist-btn'),
+  }
+}
+
+// const { props } = setup();
+const { InteractionIcons } = setup();
+const { InteractionIconsDiv } = setup();
+const { LikeBtn } = setup();
+const { DislikeBtn } = setup();
+const { PlaylistBtn } = setup();
+
+
+function renderTester(name, component) {
+  test(`${name} component`, () => {
+      expect(component.exists()).toBe(true);
+    })
+}
+
+describe('InteractionIcons.js', () => {
+  describe('renders', () => {
+    renderTester('InteractionIcons', InteractionIcons)
+    renderTester('InteractionIconsDiv', InteractionIconsDiv)
+    renderTester('LikeBtn', LikeBtn)
+    renderTester('DislikeBtn', DislikeBtn)
+    renderTester('PlaylistBtn', PlaylistBtn)
+  })
+})
